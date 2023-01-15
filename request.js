3075e3506801247741283b6959d7133b4a2c3d42
son0017 <soneec.017@mail.com>
2023-01-15 16:25:29 +0500

    changed

--- request.js
+++ request.js
@@ -1,13 +1,19 @@
+const overly = document.getElementById("overlay");
+
 function getData(resource) {
   return new Promise((resolve, reject) => {
     let request = new XMLHttpRequest();
     request.addEventListener("readystatechange", () => {
       if ((request.status == 200) & (request.readyState == 4)) {
         let data = JSON.parse(request.responseText);
+        overly.classList.add("hidden");
         resolve(data);
       } else if (request.readyState == 4) {
+        overly.classList.add("hidden");
         console.log("Something went wrong");
         reject("Something went wrong");
+      } else {
+        overly.classList.remove("hidden");
       }
     });
     request.open("get", resource);
